744dde63a612c3d31f5e596e869c9e327894d698
viratmaladaeng <102396204+viratmaladaeng@users.noreply.github.com>
2025-02-27 16:44:19 +0700

    Update main.py4

--- main.py
+++ main.py
@@ -115,9 +115,9 @@ def handle_message(event):
             ],
             "max_tokens": 800,
             "temperature": 0.0,
-	        "top_p":0.7,
-	        "frequency_penalty":0.8,  
-            "presence_penalty":0.1,
+	        "top_p":0.95,
+	        "frequency_penalty":0,  
+            "presence_penalty":0,
 	        "stop": ["เริ่มการสนทนาใหม่", "admin", "ผู้ดูแลระบบ","ไม่มีข้อมูลในระบบ"],  # เพิ่มคำที่ต้องการให้ AI หยุดเมื่อพบ
             "stream":False  
         }
